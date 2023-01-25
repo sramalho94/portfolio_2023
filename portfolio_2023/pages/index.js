061e2619f64dd2d31f41d5d41a6086b1e7c95773
Stephan D Ramalho <sramalho@fordham.edu>
2023-01-25 14:29:32 -0500

    added nav bar

--- portfolio_2023/pages/index.js
+++ portfolio_2023/pages/index.js
@@ -1,4 +1,5 @@
 import Head from 'next/head'
+import { BsFillMoonStarsFill } from 'react-icons/bs'
 
 export default function Home() {
   return (
@@ -9,7 +10,26 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main></main>
+      <main className="bg-white px-10">
+        <section className="min-h-screen">
+          <nav className="py-10 mb-12 flex justify-between">
+            <h1 className="text-xl">sramalho94</h1>
+            <ul className="flex items-center">
+              <li>
+                <BsFillMoonStarsFill className="cursor-pointer text-xl" />
+              </li>
+              <li>
+                <a
+                  className="bg-cyan-500 text-white px-4 py-2 rounded-md ml-8"
+                  href=""
+                >
+                  Resume
+                </a>
+              </li>
+            </ul>
+          </nav>
+        </section>
+      </main>
     </div>
   )
 }
