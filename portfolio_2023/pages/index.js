2393bac3170e0fcc8cf07e37e9419f08186dc5bc
Stephan D Ramalho <sramalho@fordham.edu>
2023-01-25 14:34:05 -0500

    added gradient to resume button

--- portfolio_2023/pages/index.js
+++ portfolio_2023/pages/index.js
@@ -20,7 +20,7 @@ export default function Home() {
               </li>
               <li>
                 <a
-                  className="bg-cyan-500 text-white px-4 py-2 rounded-md ml-8"
+                  className="bg-gradient-to-r from-cyan-500 to-teal-500 text-white px-4 py-2 rounded-md ml-8"
                   href=""
                 >
                   Resume
