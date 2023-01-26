cbec92dcb384d6d29be5b9dcfe131847c8650700
Stephan D Ramalho <sramalho@fordham.edu>
2023-01-25 19:55:02 -0500

    changes to about me text

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "c51855d66d05b0ef"; }
+/******/ 		__webpack_require__.h = function() { return "0ac36a9356edea59"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
