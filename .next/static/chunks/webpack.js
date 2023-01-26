93832e06ff5523573d2ce8d4a6d90f3375f0c3ec
Stephan D Ramalho <sramalho@fordham.edu>
2023-01-25 19:41:20 -0500

    minor additions of gradients and coloring

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "506126d7fc26fb0c"; }
+/******/ 		__webpack_require__.h = function() { return "c51855d66d05b0ef"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
