af91bfd98dba1d05d5da374672493ab81cbe9174
Stephan D Ramalho <sramalho@fordham.edu>
2023-03-18 13:14:51 -0400

    created file to store data for projects

---
+++ projectsData.js
@@ -0,0 +1,80 @@
+const projects = [
+  {
+    image: '/Allstate_screenshot.png',
+    name: 'Allstate Carpets',
+    description: `I led a software development project that involved designing a web application for a client using Next13 technology. My colleague and I applied tailwindcss styling to enhance the user interface and incorporated a touch-enabled slideshow to improve user experience. We deployed the application on Amazon Web Services (AWS) and attached it to a custom domain that the customer had previously purchased. We followed a professional and technical approach to ensure the application met the client's requirements and adhered to industry best practices. Our aim was to deliver a high-quality web application that met the client's needs and exceeded their expectations.`,
+    projectLink: 'https://www.allstatecarpets.com/',
+    githubLink: 'https://github.com/jcarr048/AllstateCarpets',
+    technologies: ['aws', 'next', 'tailwind']
+  },
+  {
+    image: '/PokePro.png',
+    name: 'PokePro',
+    description: `In this final project for General Assembly's Software
+    Engineering Immersive, I made a full stack web application that
+    allows users to look up trainers in the base pokemon games and
+    see their pokemon team. Suggestions are given on how to beat
+    each pokemon based on that Pokemon's type.`,
+    projectLink: 'https://pokepro-frontend.herokuapp.com/',
+    githubLink: 'https://github.com/sramalho94/PokePro',
+    technologies: [
+      'javascript',
+      'tailwind',
+      'postgres',
+      'express',
+      'react',
+      'node'
+    ]
+  },
+  {
+    image: '/faults2.png',
+    name: 'The Faults In Our Stars',
+    description: `The Faults In Our Stars is a hyper-personalized, social
+    experience bringing astrology into the 22nd century. Our aim is
+    to create social circles revolving around zodiac signs. This
+    website will allow you to connect to people that are compatible
+    to your zodiac sign and at the same time rate the users based on
+    their personalities and sign compatibility; wether good or bad,
+    create new social circles, that will improve you work and love
+    life.`,
+    projectLink: 'https://starz-app.herokuapp.com/',
+    githubLink:
+      'https://github.com/BrianDLara/The-Faults-In-Our-Stars_Frontend',
+    technologies: ['javascript', 'css', 'postgres', 'express', 'react', 'node']
+  },
+  {
+    image: '/cornhub.png',
+    name: 'Corney Island',
+    description: `In this group hackathon project, our group created a theme
+    parked themed web page. There are a number of themes and
+    attractions to navigate through as you explore the page. The
+    user also has the capability to add and delete attractions as
+    well.`,
+    projectLink: 'https://corney-island2022.herokuapp.com/',
+    githubLink: 'https://github.com/sramalho94/Corney_Island_Group_Project',
+    technologies: [
+      'javascript',
+      'css',
+      'html',
+      'mongodb',
+      'express',
+      'react',
+      'node'
+    ]
+  },
+  {
+    image: '/battleships.png',
+    name: `Admiral Stephan's BattleShips`,
+    description: `In this first project, I created a web browser version of the
+      classic board game, Battleship. Using only vanilla JavaScript I
+      created a fully functional game where the player goes against an
+      AI. When either the player or the AI runs out of ships, the game
+      is over.`,
+    projectLink: 'https://admiralstephanbattleship.surge.sh/',
+    githubLink:
+      'https://github.com/sramalho94/Admiral-Stephan-Battleship-Project',
+    technologies: ['javascript', 'css', 'html']
+  }
+]
+
+export default projects
