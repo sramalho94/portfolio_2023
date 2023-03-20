3a7acd4637a7522a6fcabb9370bbf1ee4bc5fe23
Stephan D Ramalho <sramalho@fordham.edu>
2023-03-20 13:17:58 -0400

    moved previous method of displaying projects into seperate file to demonstrate progress

---
+++ oldportfolio.js
@@ -0,0 +1,57 @@
+;<div
+  className="basis-1/3 flex-1 shadow-xl bg-gradient-to-tr from-emerald-200 rounded-lg"
+  id="project4"
+>
+  <Image
+    src={pokepro}
+    className="rounded-lg object-cover"
+    width={'100%'}
+    height={'100%'}
+    layout="responsive"
+  />
+  <p className="text-xl font-bold pt-8 pb-2 text-center  dark:text-white">
+    PokePro
+  </p>
+  <p className="py-2 text-center dark:text-white">
+    In this final project for General Assembly's Software Engineering Immersive,
+    I made a full stack web application that allows users to look up trainers in
+    the base pokemon games and see their pokemon team. Suggestions are given on
+    how to beat each pokemon based on that Pokemon's type.
+  </p>
+  <div className="flex flex-row py-2 justify-center text-purple-600 dark:text-white">
+    <a
+      href="https://pokepro-frontend.herokuapp.com/"
+      className="mr-5 font-bold text-xl hover:scale-125 transition-all duration-300"
+    >
+      Project Link
+    </a>
+    <a
+      href="https://github.com/sramalho94/PokePro"
+      className="mr-5 font-bold text-xl hover:scale-125 transition-all duration-300"
+    >
+      Github Link
+    </a>
+  </div>
+  <div>
+    <ul className="flex flex-row justify-center dark:text-white">
+      <li>
+        <SiJavascript className="text-4xl text-center mx-auto pr-3" />
+      </li>
+      <li>
+        <SiCss3 className="text-4xl text-center mx-auto pr-3" />
+      </li>
+      <li>
+        <SiPostgresql className="text-4xl text-center mx-auto pr-3" />
+      </li>
+      <li>
+        <SiExpress className="text-4xl text-center mx-auto pr-3" />
+      </li>
+      <li>
+        <SiReact className="text-4xl text-center mx-auto pr-3" />
+      </li>
+      <li>
+        <IoLogoNodejs className="text-4xl text-center mx-auto pr-3" />
+      </li>
+    </ul>
+  </div>
+</div>
