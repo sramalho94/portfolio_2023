46bd9c068856942c367b37dbc2a8ca4b21639040
Stephan D Ramalho <sramalho@fordham.edu>
2023-04-25 14:53:49 -0400

    created emailform component

---
+++ components/EmailForm.jsx
@@ -0,0 +1,96 @@
+import React, { useState } from 'react'
+
+const EmailForm = () => {
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    message: ''
+  })
+
+  const handleChange = (event) => {
+    const { name, value } = event.target
+    setFormData({ ...formData, [name]: value })
+  }
+
+  const handleSubmit = async (event) => {
+    event.preventDefault()
+
+    const msg = {
+      to: 'stephanramalho@gmail.com',
+      from: formData.email,
+      subject: `New message from ${formData.name}`,
+      text: formData.message
+    }
+
+    const response = await fetch('/api/send-email', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(msg)
+    })
+
+    if (response.ok) {
+      alert('Email sent successfully!')
+    } else {
+      console.error(await response.json())
+      alert('Failed to send email.')
+    }
+  }
+
+  return (
+    <div className="flex items-center min-h-screen">
+      <form onSubmit={handleSubmit} className="w-full">
+        <div className="mb-4">
+          <label htmlFor="name" className="block text-sm font-medium">
+            Name
+          </label>
+          <input
+            type="text"
+            id="name"
+            name="name"
+            value={formData.name}
+            onChange={handleChange}
+            className="mt-1 w-full p-2 border border-gray-300 rounded"
+            required
+          />
+        </div>
+        <div className="mb-4">
+          <label htmlFor="email" className="block text-sm font-medium">
+            Email
+          </label>
+          <input
+            type="email"
+            id="email"
+            name="email"
+            value={formData.email}
+            onChange={handleChange}
+            className="mt-1 w-full p-2 border border-gray-300 rounded"
+            required
+          />
+        </div>
+        <div className="mb-4">
+          <label htmlFor="message" className="block text-sm font-medium">
+            Message
+          </label>
+          <textarea
+            id="message"
+            name="message"
+            value={formData.message}
+            onChange={handleChange}
+            className="mt-1 w-full p-2 border border-gray-300 rounded"
+            required
+          ></textarea>
+        </div>
+        <button
+          type="submit"
+          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+        >
+          Send
+        </button>
+      </form>
+    </div>
+  )
+}
+
+export default EmailForm
