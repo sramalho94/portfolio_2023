93f26b908922681dbf4149fb11f1a8c42dde7cda
Stephan D Ramalho <sramalho@fordham.edu>
2023-09-12 10:06:18 -0400

    refactored project to use App Router instead of Pages directory

---
+++ app/layout.jsx
@@ -0,0 +1,17 @@
+import '@/styles/globals.css'
+
+export default function RootLayout({
+  // Layouts must accept a children prop.
+  // This will be populated with nested layouts or pages
+  children
+}) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  )
+}
+export const metadata = {
+  title: 'Stephan D Ramalho Portfolio',
+  description: 'Welcome to my portfolio'
+}
